0775017867b1003533afb7fb0f8c1f79c9ba970f
verydecent <wnjhwng@gmail.com>
2019-06-19 22:24:31 -0700

    Didn't use port into server listener

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`Server listening at Port ${port}`);
 });
