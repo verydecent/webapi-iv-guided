8cfd6aebd22592b9c9185b7852d7a57f9881e28a
verydecent <wnjhwng@gmail.com>
2019-06-19 22:40:31 -0700

    update

--- api/server.js
+++ api/server.js
@@ -11,7 +11,8 @@ server.use(express.json());
 server.get('/', async (req, res) => {
   try {
     const shoutouts = await db('shoutouts');
-    res.status(200).json(shoutouts);
+    const messageOfTheDay = process.env.MOTD || "Hello Ya'll";
+    res.status(200).json({ motd: messageOfTheDay, shoutours });
   } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
